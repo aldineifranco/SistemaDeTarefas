1fcca703d6eb4e9b12eb219e2d861a4cd1b7686a
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-10-28 07:55:43 -0300

    upp

---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContext _dbContext;
+
+        public UsuarioRepositorio(SistemaTarefasDBContext sistemaTarefasDBContext)
+        {
+            _dbContext = sistemaTarefasDBContext;
+        }
+
+
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Usuario.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContext.Usuario.ToListAsync();
+        }
+
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbContext.Usuario.AddAsync(usuario);
+            await _dbContext.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null) 
+            {
+                throw new Exception($"Usuario para o ID: {id} não foi encontrado.");
+            }
+
+            usuarioPorId.Nome = usuario.Nome;
+            usuarioPorId.Email = usuario.Email;
+
+            _dbContext.Usuario.Update(usuarioPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuario para o ID: {id} não foi encontrado.");
+            }
+
+            _dbContext.Usuario.Remove(usuarioPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+    }
+}
